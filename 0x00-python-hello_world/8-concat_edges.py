cb64486d9d433b5a12480029e9a913839e64361f
Happiness <adamhappinessjw@gmail.com>
2024-03-11 18:19:28 -0700

    task 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "python is an interpreted, interactive, object_oriented programming\
+langauge that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str) 
+
