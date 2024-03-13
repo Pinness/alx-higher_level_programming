055bd3af5f814b5b0a8c7e70688d2437e4946a3a
Happiness <adamhappinessjw@gmail.com>
2024-03-13 15:31:52 -0700

    ifelse

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,12 +4,13 @@ number = random.randint(-10000, 10000)
 
 ran_number = str(number)
 ran_number = ran_number[-1:]
-if ran_number > '5' and number > 0: 
-    print("Last digit of {} is {} and is greater than 5".format(number, ran_number))
+num_str = "Last digit of {} is {} "
+if ran_number > '5' and number > 0:
+    print(num_str.format(number, ran_number) + "and is greater than 5")
 elif ran_number == '0':
-    print("Last digit of {} is {} and is zero".format(number, ran_number))
-elif ran_number < '6' and  ran_number != '0' and number > 0: 
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, ran_number))
-
-elif ran_number != '0' and number < 0:
-    print("Last digit of {} is -{} and is less than 6 and not 0".format(number, ran_number))
+    print(num_str.format(number, ran_number) + "and is 0")
+elif ran_number < '6' and ran_number != '0' and number > 0:
+    print(num_str.format(number, ran_number) + "and is less than 6 and not 0")
+num_str = "Last digit of {} is -{} "
+if ran_number != '0' and number < 0:
+    print(num_str.format(number, ran_number) + "and is less than 6 and not 0")
